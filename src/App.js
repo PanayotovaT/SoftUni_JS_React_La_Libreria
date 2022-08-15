3cd4ca61d9ff33968b79a7656ae65c1d9a2133dd
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-08-15 15:35:31 +0300

    Added Home component.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './components/Header/Header';
 import Login from './components/Login/Login';
 import Footer from './components/Footer/Footer';
 import Register from './components/Register/Register';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register/>} />
       </Routes>
