595ff03cf0b1f70c0e4544485cf2ea95f4f27379
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-08-17 12:16:14 +0300

    Update functionality added.

--- src/components/UpdateBook/UpdateBook.js
+++ src/components/UpdateBook/UpdateBook.js
@@ -1,36 +1,78 @@
+import { useState, useEffect} from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import * as bookService from '../../services/bookService';
+
+import { useAuthContext } from '../../contexts/AuthContext';
+
 import './UpdateBook.css';
 
 const UpdateBook = () => {
+    const navigate = useNavigate();
+    const [book, setBook] = useState({});
+    const {bookId } = useParams();
+
+    useEffect(() => {
+        bookService.getOne(bookId)
+            .then(res => {
+                setBook(res);
+            })
+            .catch(err => {
+                console.error(err);
+                navigate(`/details/${bookId}`)
+            })
+    },[]);
+
+    const updateHandler =(e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const {title, category, imageUrl, price, author, description} = Object.fromEntries(formData);
+
+        if(title == '' || category == '' || imageUrl == '' || price == '' || author == '' || description == '') {
+            throw new Error('All fields are required');
+        }
+        const updatedBook = {title, category, imageUrl, price, author, description};
+
+        bookService.update(bookId, updatedBook)
+            .then(res => {
+                navigate(`/details/${bookId}`);
+            }).catch(err => {
+                console.error(err.message);
+                return;
+            })
+
+    }
 
     return (
         <section className="update-section">
-        <form className="update-form" method="POST">
+        <form className="update-form" method="POST" onSubmit={updateHandler}>
             <fieldset className="update-form-fieldset">
                 <legend className="update-form-legend" >Update Book</legend>
                     <div className="update-form-field" >
                         <label htmlFor="title">Title</label>
-                        <input type="text" id="title" name="title" />
+                        <input type="text" id="title" name="title" defaultValue={book.title} />
                     </div>
                     <div className="update-form-field" >
                         <label htmlFor="category">Category</label>
-                        <input type="text" id="category" name="category" />
+                        <input type="text" id="category" name="category" defaultValue={book.category}/>
                     </div>
                   
                     <div className="update-form-field" >
                         <label htmlFor="image">Iamge</label>
-                        <input type="text" id="image" name="imageUrl" />
+                        <input type="text" id="image" name="imageUrl" defaultValue={book.imageUrl} />
                     </div>
                     <div className="update-form-field" >
                         <label htmlFor="price">Price</label>
-                        <input type="number" id="price" name="price" />
+                        <input type="number" id="price" name="price" defaultValue={book.price} />
                     </div>
                     <div className="update-form-field" >
                         <label htmlFor="author">Author</label>
-                        <input type="text" id="author" name="author" />
+                        <input type="text" id="author" name="author" defaultValue={book.author} />
                     </div>
                     <div className="update-form-field" >
                         <label htmlFor="description">Description</label>
-                        <textarea type="text" id="description" name="description" />
+                        <textarea type="text" id="description" name="description" defaultValue={book.description} />
                     </div>
                     <div className="update-form-field" >
                         <input type="submit" className="update-btn" value="Update Book" />
--- src/services/bookService.js
+++ src/services/bookService.js
@@ -4,12 +4,12 @@ const baseUrl= 'https://softuni-custom-server-test.herokuapp.com/data/books';
 
 export const getAll = async () =>await request.get(baseUrl);
 
-export const getOne = async ( postId ) => await request.get(`${baseUrl}/${postId}`);
+export const getOne = async ( bookId ) => await request.get(`${baseUrl}/${bookId}`);
 
 export const create = async ( data ) => await request.post(baseUrl, data);
 
-export const update = async ( postId, data ) => await request.put(`${baseUrl}/${postId}`, data);
+export const update = async ( bookId, data ) => await request.put(`${baseUrl}/${bookId}`, data);
 
-export const deleteBook = async ( postId ) => await request.deleteItem(`${baseUrl}/${postId}`);
+export const deleteBook = async ( bookId ) => await request.deleteItem(`${baseUrl}/${bookId}`);
 
 export const getMyBooks =  async ( userId ) =>  await request.get(`${baseUrl}?where=_ownerId%3D%22${userId}%22`);
