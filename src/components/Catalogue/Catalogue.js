e62079de6cd9e220c0f6fb24413d3cbb7882174c
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-08-17 10:04:51 +0300

    Catalogue data added.

--- src/components/Catalogue/Catalogue.js
+++ src/components/Catalogue/Catalogue.js
@@ -1,21 +1,41 @@
-import './Catalogue.css';
+import { useState, useEffect } from 'react';
+
+import * as bookService from '../../services/bookService';
+
 import CatalogueCard from './CatalogueCard';
+import './Catalogue.css';
+
 const Catalogue = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        bookService.getAll()
+            .then(data => {
+                setBooks(Object.values(data))
+            })
+            .catch(err => {
+                console.log(err.message);
+                return;
+            })
+    }, []);
 
     return (
         <section className="catalogue-section">
             <div className="catalogue-colection">
                 <h1 className="catalogue-colection-title">Welcome to the Books Catalogue</h1>
                 <ul className="catalogue-colection-list">
-                        <CatalogueCard />
+                    {books.length > 0
+                        ? books.map(x => <CatalogueCard key="x._id" card={x} />)
+                        : (<div className="catalogue-nodata">
+                            <h2 className="catalogue-nodata-msg">No Books in database!</h2>
+                            <article className="catalogue-nodata-article-img">
+                                <img className="catalogue-nodata-img" src="https://cdn.elearningindustry.com/wp-content/uploads/2016/05/top-10-books-every-college-student-read-1024x640.jpeg" alt="no-database" />
+                            </article>
+                        </div>)
+                    }
                 </ul>
             </div>
-            <div className="catalogue-nodata">
-                <h2 className="catalogue-nodata-msg">No Books in database!</h2>
-                <article className="catalogue-nodata-article-img">
-                    <img className="catalogue-nodata-img"src="https://cdn.elearningindustry.com/wp-content/uploads/2016/05/top-10-books-every-college-student-read-1024x640.jpeg" alt="no-database" />
-                </article>
-            </div>
+
         </section>
     )
 }
