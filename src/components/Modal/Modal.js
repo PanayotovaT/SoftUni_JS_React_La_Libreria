dca4d433a3589b1b6b95dc7f4d5ce0030b1af179
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-08-27 23:51:41 +0300

    Adding css to the modal.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,31 +1,34 @@
-import { useState } from 'react';
+import './Modal.css';
 
 
-const Modal = ({ 
+const Modal = ({
   closeModal,
   deleteHandler,
   book
- }) => {
+}) => {
 
 
   return (
     <div className="modal-background">
-      <div className="modal-header">
-        <div className="header-btn-container">
-          <button className="x-btn" onClick={() => closeModal(false)}> X </button>
+      <div className="modal-container">
+
+        <div className="modal-header">
+          <div className="header-btn-container">
+            <button className="x-btn" onClick={() => closeModal(false)}> X </button>
+          </div>
+          <div className="header-title-container">
+            <h2>Are you sure you want to delete the following book?</h2>
+          </div>
         </div>
-        <div className="header-title-container">
-          <h2>Are you sure you want to delete the following book?</h2>
-        </div>
-      </div>
-      <div className="modal-body">
+        <div className="modal-body">
           <p>Title: {book.title}</p>
           <p>Category: {book.category}</p>
           <p>Author: {book.author}</p>
-      </div>
-      <div className="modal-footer">
-        <button onClick={() => closeModal(false)}> Cancel </button>
-        <button onClick={() =>deleteHandler(book._id)}> Delete Book</button>
+        </div>
+        <div className="modal-footer">
+          <button onClick={() => closeModal(false)} className="cancel-btn"> Cancel </button>
+          <button onClick={() => deleteHandler(book._id)}> Delete Book</button>
+        </div>
       </div>
     </div>
   );
