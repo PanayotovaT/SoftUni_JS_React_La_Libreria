076ecc9963ccee3a7c65654a42b2c004a5dda423
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-08-27 23:13:25 +0300

    Updated modal text.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -15,7 +15,7 @@ const Modal = ({
           <button className="x-btn" onClick={() => closeModal(false)}> X </button>
         </div>
         <div className="header-title-container">
-          <h2>Are you sure you want to delete this item?</h2>
+          <h2>Are you sure you want to delete the following book?</h2>
         </div>
       </div>
       <div className="modal-body">
@@ -25,7 +25,7 @@ const Modal = ({
       </div>
       <div className="modal-footer">
         <button onClick={() => closeModal(false)}> Cancel </button>
-        <button onClick={() =>deleteHandler(book._id)}> Delete Item</button>
+        <button onClick={() =>deleteHandler(book._id)}> Delete Book</button>
       </div>
     </div>
   );
