a19558c4368c2f0ec3ebc3ffa0d222fe0d093d7a
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-08-29 01:16:17 +0300

    Added spinner to MyProfile

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -1,3 +1,4 @@
+import BeatLoader from "react-spinners/BeatLoader";
 import { useState, useEffect } from 'react';
 
 import * as bookService from '../../services/bookService';
@@ -7,19 +8,32 @@ import ProfileCard from './ProfileCard';
 import './MyProfile.css';
 
 const MyProfile = () => {
+    const [loader, setLoader ] = useState(false);
     const [books, setBooks] = useState([]);
     const { user } = useAuthContext();
 
     useEffect(() => {
+        setLoader(true);
+
         bookService.getMyBooks(user._id)
             .then(data => {
-                setBooks(Object.values(data))
+                setBooks(Object.values(data));
+                setLoader(false);
             })
             .catch(err => {
                 console.log(err.message);
                 return;
             })
     }, [user._id]);
+
+    const booksSection = books.length > 0
+        ? books.map(x => <ProfileCard key="x._id" card={x} />)
+        : (<div className="profile-nodata">
+            <h2 className="profile-nodata-msg">No Books in database!</h2>
+            <article className="profile-nodata-article-img">
+                <img className="profile-nodata-img" src="https://cdn.elearningindustry.com/wp-content/uploads/2016/05/top-10-books-every-college-student-read-1024x640.jpeg" alt="no-database" />
+            </article>
+        </div>);
 
     return (
         <section className="profile-section">
@@ -28,14 +42,9 @@ const MyProfile = () => {
                 <h3>{user.firstName} {user.lastName}</h3>
                 <p>{user.email}</p>
                 <ul className="profile-colection-list">
-                    {books.length > 0
-                        ? books.map(x => <ProfileCard key="x._id" card={x} />)
-                        : (<div className="profile-nodata">
-                            <h2 className="profile-nodata-msg">No Books in database!</h2>
-                            <article className="profile-nodata-article-img">
-                                <img className="profile-nodata-img" src="https://cdn.elearningindustry.com/wp-content/uploads/2016/05/top-10-books-every-college-student-read-1024x640.jpeg" alt="no-database" />
-                            </article>
-                        </div>)
+                    {loader
+                        ? <BeatLoader color={'#36D7B7'} loading={loader} size={25} />
+                        : booksSection
                     }
                 </ul>
             </div>
